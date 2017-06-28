4e39fca92ff746dd55a2d81f3537257728b4fba3
patorndev <di_patorani@msn.com>
2017-06-28 20:19:28 +0700

    Done

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow(library) {
+    return <ListItem library={library} />;
+
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
-
+// return props { libraries } from state
 const mapStateToProps = state => {
   return { libraries: state.libraries };
 };
 
-// connect() return another function, then call that function with LibraryList
+// connect() return another function,
+// then call that function with LibraryList,
+// return state from reducers
 export default connect(mapStateToProps)(LibraryList);
